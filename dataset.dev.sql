6b2610d7ef27ce3762cf3d704286fbfb9dc5acd2
Petr Masopust <petr.masopust@gooddata.com>
2019-11-07 09:13:51 +0100

    Development dataset

---
+++ dataset.dev.sql
@@ -0,0 +1,10 @@
+select|Position|from table|where|School/Club Team|=|Butler CC (KS)
+select|COUNT|School/Club Team|from table|where|No.|=|3
+select|School/Club Team|from table|where|No.|=|21
+select|Player|from table|where|No.|=|42
+select|Player|from table|where|Position|=|Guard|AND|Years in Toronto|=|1996-97
+select|Player|from table|where|School/Club Team|=|Westchester High School
+select|School/Club Team|from table|where|Player|=|Amir Johnson
+select|COUNT|No.|from table|where|Years in Toronto|=|2005-06
+select|COUNT|Position|from table|where|Years in Toronto|=|2006-07
+select|Nationality|from table|where|School/Club Team|=|Fresno State
